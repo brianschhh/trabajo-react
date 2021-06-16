1f10b572f6f2a4a60e5220690c5cb639cc0c8ac2
brianschhh <brian.schmi@hotmail.com>
2021-06-16 20:34:48 -0300

    title

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -3,11 +3,24 @@ import { Button } from "semantic-ui-react";
 import "./Counter.css";
 
 function Counter() {
-  const [counter, setCounter] = useState(0);
+  let min = 1;
+  let max = 10;
+  const [counter, setCounter] = useState(min);
 
-  const counterUp = () => setCounter(counter + 1);
+  function counterUp() {
+    const newValue = counter + 1;
+    if (newValue <= max) {
+      setCounter(newValue);
+    }
+  }
 
-  const counterDown = () => setCounter(counter - 1);
+  function counterDown() {
+    const newValue = counter - 1;
+    if (newValue >= min) {
+      setCounter(newValue);
+    }
+  }
+
   return (
     <div>
       <div className="SectionCounter">
